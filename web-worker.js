555ca105ec744f6bc0bae17d9b4ba56e0d8c1905
dickaestu <dickaestusaputra@gmail.com>
2021-12-29 15:18:35 +0700

    web-worker

---
+++ web-worker.js
@@ -0,0 +1,10 @@
+function showLog(total) {
+  for (let i = 0; i < total; i++) {
+    console.log(i);
+  }
+}
+addEventListener("message", (event) => {
+  const total = event.data;
+  showLog(total);
+  postMessage("Done");
+});
